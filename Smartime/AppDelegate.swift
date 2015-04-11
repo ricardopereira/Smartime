4849e3dfc26ef90209f33a6ca499cb6a2270cd85
Ricardo Pereira <m@ricardopereira.eu>
2015-04-11 02:21:50 +0100

    Open connection

--- Smartime/AppDelegate.swift
+++ Smartime/AppDelegate.swift
@@ -48,27 +48,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func test() {
+        /*
+        struct JSONValidator<T> {
+        
+            typealias ValidationClosure = (JSON) -> Bool
+        
+            let validator: ValidationClosure
+        
+            init(validator: ValidationClosure) {
+               self.validator = validator
+            }
+        
+            func isValid(json: JSON) -> Bool {
+                return validator(json)
+            }
+        
+        }
+        
+        struct MyModel {
+            let name: String
+            let number: Int
+            let date: NSDate
+        
+            init(name: String, number: Int, date: NSDate)
+        }
+        
+        protocol JSONParserType {
+            typealias T
+            func parseJSON(json: JSON) -> T
+        }
+        
+        struct MyModelParser: JSONParserType {
+        
+            func parseJSON(json: JSON) -> MyModel {
+                // parse json and construct MyModel
+                return MyModel(name: "name", number: 1, date: NSDate())
+            }
+        
+        }
+        
+        let json = JSON(data: dataFromServer)
+        
+        let validator = JSONValidator<MyModel> { (json) -> Bool in
+            // validate the json
+            return true
+        }
+        
+        if !validator.isValid(json) {
+            // handle bad json
+            return
+        }
+        
+        let parser = MyModelParser()
+        
+        let myModel = parser.parseJSON(json)
+        */
     }
 
     func start() -> UIViewController {
