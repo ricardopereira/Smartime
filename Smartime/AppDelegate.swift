e279e04be23a52a2e6e9f62f18c6dc885985079c
Ricardo Pereira <m@ricardopereira.eu>
2015-05-09 15:06:08 +0100

    Some test buttons

--- Smartime/AppDelegate.swift
+++ Smartime/AppDelegate.swift
@@ -12,18 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var socket: SocketIO!
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = start()
         
-        let socket = SocketIO(url: "http://localhost:8000/");
+        socket = SocketIO(url: "http://localhost:8000/");
         
-        socket.on("myevent") { (arg: SocketIOArg) -> (SocketIOResult) in
-            //println("Event: \(arg)")
-            return SocketIOResult.Success(status: 0)
-        }.on(.ConnectError) { (arg: SocketIOArg) -> (SocketIOResult) in
+        socket.on(.ConnectError) { (arg: SocketIOArg) -> (SocketIOResult) in
             switch arg {
             case .Failure(let error):
                 println(error)
@@ -33,10 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return SocketIOResult.Success(status: 0)
         }.on(.Connected) { (arg: SocketIOArg) -> (SocketIOResult) in
             // Connected
-            // Emit "connect" not possible!
-            socket.emit("myevent", withMessage: "sdflkaskdçfjwe")
-            socket.off()
-            socket.emit("myevent", withMessage: "aaaa")
+            // ToDo: Emit "connect" not possible!
+            
+            //socket.emit("myevent", withMessage: "sdflkaskdçfjwe")
+            //socket.off()
+            //socket.emit("myevent", withMessage: "aaaa")
             
             return SocketIOResult.Success(status: 0)
         }
@@ -51,76 +50,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return SocketIOResult.Success(status: 0)
         })
         
-        socket.connect()
-        
-        // Teste
-        
-        //socket.emit("connect", withMessage: "200")
-
         return true
     }
     
-    func test() {
-        /*
-        struct JSONValidator<T> {
-        
-            typealias ValidationClosure = (JSON) -> Bool
-        
-            let validator: ValidationClosure
-        
-            init(validator: ValidationClosure) {
-               self.validator = validator
-            }
-        
-            func isValid(json: JSON) -> Bool {
-                return validator(json)
-            }
-        
-        }
-        
-        struct MyModel {
-            let name: String
-            let number: Int
-            let date: NSDate
-        
-            init(name: String, number: Int, date: NSDate)
-        }
-        
-        protocol JSONParserType {
-            typealias T
-            func parseJSON(json: JSON) -> T
-        }
-        
-        struct MyModelParser: JSONParserType {
-        
-            func parseJSON(json: JSON) -> MyModel {
-                // parse json and construct MyModel
-                return MyModel(name: "name", number: 1, date: NSDate())
-            }
-        
-        }
-        
-        let json = JSON(data: dataFromServer)
-        
-        let validator = JSONValidator<MyModel> { (json) -> Bool in
-            // validate the json
-            return true
-        }
-        
-        if !validator.isValid(json) {
-            // handle bad json
-            return
-        }
-        
-        let parser = MyModelParser()
-        
-        let myModel = parser.parseJSON(json)
-        */
-    }
-
     func start() -> UIViewController {
         let mainView = UIViewController()
-        mainView.view.backgroundColor = UIColor.greenColor()
+        mainView.view.backgroundColor = UIColor.blueColor()
+
+        let connectButton = UIButton(frame: CGRectMake(20, 40, 70, 30))
+        connectButton.setTitle("Connect", forState: .Normal)
+        
+        let disconnectButton = UIButton(frame: CGRectMake(20, 120, 100, 30))
+        disconnectButton.setTitle("Disconnect", forState: .Normal)
+        
+        let sendButton = UIButton(frame: CGRectMake(20, 80, 70, 30))
+        sendButton.setTitle("Send", forState: .Normal)
+        sendButton.layer.cornerRadius = 3.5
+        sendButton.layer.borderWidth = 1.0
+        sendButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        connectButton.addTarget(self, action: Selector("didTouchConnect:"), forControlEvents: .TouchUpInside)
+        disconnectButton.addTarget(self, action: Selector("didTouchDisconnect:"), forControlEvents: .TouchUpInside)
+        sendButton.addTarget(self, action: Selector("didTouchSend:"), forControlEvents: .TouchUpInside)
+        
+        mainView.view.addSubview(connectButton)
+        mainView.view.addSubview(disconnectButton)
+        mainView.view.addSubview(sendButton)
         return mainView
+    }
+    
+    func didTouchConnect(sender: AnyObject?) {
+        socket.connect()
+    }
+    
+    func didTouchDisconnect(sender: AnyObject?) {
+        
+    }
+    
+    func didTouchSend(sender: AnyObject?) {
+        socket.emit("chat message", withMessage: "I'm iOS")
     }
 }
--- Smartime/MainViewController.swift
+++ Smartime/MainViewController.swift
@@ -13,8 +13,10 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
+    override func loadView() {
+        
+    }
 
 }
